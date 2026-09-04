60d02be850b0c6e96629193148518062239917f2
jaysarode2007 <jay.sarode003@gmail.com>
2026-09-04 23:26:41 +0530

    Day 12 - Squares of a Sorted Array

---
+++ Day-12/SquaresOfASortedArray.java
@@ -0,0 +1,32 @@
+// LeetCode 977 - Squares of a Sorted Array
+// Day 12 of my 100 Days DSA Challenge
+
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+
+        int[] ans = new int[nums.length];
+
+        int start = 0;
+        int end = nums.length - 1;
+
+        int ptr = ans.length - 1;
+
+        while (start <= end) {
+
+            int ss = nums[start] * nums[start];
+            int es = nums[end] * nums[end];
+
+            if (ss >= es) {
+                ans[ptr] = ss;
+                start++;
+            } else {
+                ans[ptr] = es;
+                end--;
+            }
+
+            ptr--;
+        }
+
+        return ans;
+    }
+}
