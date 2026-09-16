77d3910353e0ce99c194471d41cbe23672fc9d8f
jaysarode2007 <jay.sarode003@gmail.com>
2026-09-16 23:10:11 +0530

    Day 18 - Increasing Triplet Subsequence

---
+++ Day-18/IncreasingTripletSubsequence.java
@@ -0,0 +1,25 @@
+// LeetCode 334 - Increasing Triplet Subsequence
+// Day 18 of my 100 Days DSA Challenge
+
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int first = Integer.MAX_VALUE ;
+        int second = Integer.MAX_VALUE ;
+        int third = Integer.MAX_VALUE ;
+
+        for(int i = 0; i<nums.length; i++){
+
+            int element = nums[i] ;
+
+            if(first >= element){
+                first = element ;
+            }else if(second >= element){
+                second = element ;
+            }else{
+                third = element ;
+                return true ;
+            }
+        }
+        return false ;
+    }
+}
